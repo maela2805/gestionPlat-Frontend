1a4de789d23c23e0cc5e40633332ff28a8afc67a
maelmaela <mael-maela.koutoglo@ism.edu.sn>
2026-07-22 14:48:29 +0000

    Configuration API production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://gestionplat-backend.onrender.com',
   cloudinary: {
     cloudName: 'dfa4e7afk',
     uploadPreset: 'gestionPlat'
