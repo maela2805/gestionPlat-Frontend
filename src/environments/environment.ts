6a257ccc62ab09e52aa31bd6db2062267ece41ff
maelmaela <mael-maela.koutoglo@ism.edu.sn>
2026-07-28 13:30:05 +0000

    fix(environment): configure local apiUrl to http://localhost:8081 for local development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://gestionplat-backend.onrender.com',
+  apiUrl: 'http://localhost:8081',
   cloudinary: {
     cloudName: 'dfa4e7afk',
     uploadPreset: 'gestionPlat'
